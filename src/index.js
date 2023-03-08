3a43948700498cf9938eebbebaaa638ac50d4bef
Yan <kesselyanniel@gmail.com>
2023-03-08 00:33:17 -0500

    Update index.js
    Update the backend url api

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import Pages from "./pages";
 import { ApolloProvider, ApolloClient, InMemoryCache } from "@apollo/client";
 
 const client = new ApolloClient({
-  uri: "https://odyssey-lift-off-part5-server-production-2eef.up.railway.app/", // change to YOUR own production server
+  uri: "https://odyssey-server.up.railway.app/", // change to YOUR own production server
   cache: new InMemoryCache(),
   name: "web",
   version: "1.0",
